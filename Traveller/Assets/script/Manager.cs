bb4eae87b16e7e98e1cbc14d3bf71b8b1a207087
Nyukiland <roman211113@gmail.com>
2023-07-29 20:32:35 +0200

    asset ready

--- Traveller/Assets/script/Manager.cs
+++ Traveller/Assets/script/Manager.cs
@@ -7,35 +7,42 @@ public class Manager : MonoBehaviour
     [Header ("Difficulty control")]
     [Space(2)]
     [Tooltip("1 is easy, 2 is normal, 3 is hard")] public static int difficulty = 1;
+    [SerializeField] int matrixEasy;
+    [SerializeField] int matrixNormal;
+    [SerializeField] int matrixHard;
     /*
-    Those matrix are filled with int each representing a value for the system to represent the capacity of a player
+    Those matrix (multidimensionnal array) are filled with int each representing a value for the system to represent the capacity of a player
     0 is the mountain it is interpreted as nothing can be placed
     1 is an enmpty slot where anything can be placed
     2 is a forest road where only dirt and city can be placed
     3 is dirt where only 
     */
     [Space (10)]
-    [Tooltip("matrix are used in order to represent the playground")] [Header("matrix control")]
+    [Tooltip("matrix are used in order to represent the playground")] [Header("matrix control (read only)")]
     [Space(2)]
-    [SerializeField] int[,,,] matrixEasy;
-    [SerializeField] int[,,,,,] matrixNormal;
-    [SerializeField] int[,,,,,,,,] matrixDifficult;
+    [SerializeField] int[][] matrixGame;
 
     [Space(10)]
-    [Tooltip("set object and the quantity")] [Header("Prefab list")]
+    [Tooltip("set object for both teams")] [Header("Prefab list")]
     [Space(2)]
-    Dictionary<GameObject, int> prefabBegin;
-    Dictionary<GameObject, int> prefabForest;
-    Dictionary<GameObject, int> prefabDirt;
-    Dictionary<GameObject, int> prefabCity;
-    
+    [SerializeField] GameObject[] prefabBlue;
+
+    [Space]
+
+    [SerializeField] GameObject[] prefabRed;
+
+    [Tooltip("set the quantity for each tile in every difficulty")] [Header("Prefab list")]
+    [Space(2)]
+    [SerializeField] int[] easyQuantity;
+    [SerializeField] int[] normalQuantity;
+    [SerializeField] int[] hardQuantity;
 
     // Start is called before the first frame update
     void Start()
     {
         if(difficulty ==1)
         {
-
+            //matrixGame = new int[matrixEasy][matrixEasy];
         }
     }
 
